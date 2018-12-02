1278bdfa5c98860d6ed7cbe38fadeaed4967e12d
aleph-naught2tog <maggie@8thlight.com>
2018-12-02 13:08:35 -0600

    Day2, part2

--- lib/day_2/day_2.ex
+++ lib/day_2/day_2.ex
@@ -6,12 +6,15 @@ defmodule ElixirAdvent.Day2 do
     filename = folder
     |> Path.join(@folder)
     |> Path.join(@filename)
-    |> IO.inspect()
 
     {left, right} = part_one(filename)
-    |> IO.inspect()
 
-    IO.inspect(left * right)
+    IO.puts("-----")
+    IO.inspect({left, right}, label: "part_one, #{left * right}")
+    IO.puts("-----")
+    part_two(filename)
+    |> IO.inspect(label: "part_two")
+    IO.puts("-----")
   end
 
   defp part_one(filename) do
@@ -25,11 +28,46 @@ defmodule ElixirAdvent.Day2 do
   end
 
   defp count(a) do
-    values =
     String.to_charlist(a)
     |> Enum.group_by(fn v -> v end)
     |> Enum.reject(fn {_,v} -> length(v) < 2 end)
     |> Enum.map(fn {_,v} -> length(v) end)
     |> Enum.dedup()
+  end
+
+  defp part_two(filename) do
+    [one, two] = filename
+    |> Utilities.lines(&String.to_charlist/1)
+    |> roll()
+    |> Enum.reject(fn v -> length(v) === 0 end)
+    |> Enum.map(&to_string/1)
+
+    String.myers_difference(one, two)
+    |> Enum.filter(fn {key, _value} -> key === :eq end)
+    |> Enum.map(fn {_, value} -> value end)
+    |> Enum.reduce(fn l, r -> r <> l end)
+  end
+
+  defp roll(lines) do
+    lines
+    |> Enum.map(fn line ->
+      lines
+      |> Enum.filter(fn other_line ->
+        count = count_dissimilar_characters(line, other_line)
+        count === 1
+      end)
+    end)
+    |> Enum.dedup()
+  end
+
+  defp count_dissimilar_characters(line, other_line) do
+    line
+    |> Enum.zip(other_line)
+    |> Enum.reject(&compare_chars/1)
+    |> Enum.count()
+  end
+
+  defp compare_chars({char, other_char}) do
+    char === other_char
   end
 end
