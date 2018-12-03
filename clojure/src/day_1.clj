a12a945c8794173d5969834cc83e8a789bbe1315
aleph-naught2tog <maggie@8thlight.com>
2018-12-03 00:08:21 -0600

    day 1 in clojure

--- clojure/src/day_1.clj
+++ clojure/src/day_1.clj
@@ -2,24 +2,57 @@
   (:require [clojure.java.io :as io]))
 
 (defn read-file [filename]
-  ;; with-open is ye olde autocloseable
   (with-open [reader (io/reader filename)]
-    ;; reduce how_to_reduce_fun start_val what_to_reduce
-    (reduce conj [] (line-seq reader))))
+    (->> reader
+         (line-seq)
+         (reduce conj []))))
 
 (defn get-lines [[filename & _rest]]
-  (println filename)
-  (read-file filename))
+  (->> filename
+       (read-file)
+       (map #(Integer/parseInt %))))
 
 (defn sum-lines [lines]
   (loop [[line & rest_lines] lines
          sum 0]
     (if (some? line)
-      (recur rest_lines (+ sum (Integer/parseInt line)))
-      sum
-      )))
+      (recur rest_lines (+ sum line))
+      sum)))
+
+(defn add-at-key [map_target
+                  key-list
+                  value]
+  (let [bucket (get-in map_target key-list)]
+    (if (contains? bucket value)
+      value
+      (assoc-in map_target key-list (conj bucket value)))))
+
+(defn check-existing [existing updated]
+  (cond
+    (and (even? updated) (pos? updated)) (add-at-key existing [:even :pos] updated)
+    (and (odd? updated) (pos? updated)) (add-at-key existing [:odd :pos] updated)
+    (and (even? updated) (neg? updated)) (add-at-key existing [:even :neg] updated)
+    (and (odd? updated) (neg? updated)) (add-at-key existing [:odd :neg] updated)))
+
+(defn get-repeated-value [numbers]
+  (loop [existing {:even {:neg #{} :pos #{}}
+                   :odd {:neg #{} :pos #{}}}
+         [current & to_go] numbers
+         done []
+         sum 0]
+
+    (if (nil? current)
+      (recur existing done [] sum)
+
+      (let [updated (+ current sum)
+            result (check-existing existing updated)]
+
+        (if (number? result)
+          result
+          (recur result to_go (conj done current) updated))))))
 
 (defn -main [& args] ;; & for variadic
   (let [lines (get-lines args)]
-    (println (sum-lines lines)))
-)
+    (println "\nTotal:" (sum-lines lines))
+    (println "--- --- --- --- --- ---")
+    (println "Value:" (get-repeated-value lines))))
