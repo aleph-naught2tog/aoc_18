f54e512c08823847dee9f40b435bc89bc28c71bc
aleph-naught2tog <maggie@8thlight.com>
2018-12-02 22:00:15 -0600

    day 1 part 1

--- clojure/src/day_1.clj
+++ clojure/src/day_1.clj
@@ -1,18 +1,25 @@
-(ns day_1)
-
-(require '(clojure.java.io :as io))
+(ns day_1
+  (:require [clojure.java.io :as io]))
 
 (defn read-file [filename]
   ;; with-open is ye olde autocloseable
-  (with-open [rdr (io/reader filename)]
-    (reduce conj [] (line-seq rdr))
-  )
-)
+  (with-open [reader (io/reader filename)]
+    ;; reduce how_to_reduce_fun start_val what_to_reduce
+    (reduce conj [] (line-seq reader))))
+
+(defn get-lines [[filename & _rest]]
+  (println filename)
+  (read-file filename))
+
+(defn sum-lines [lines]
+  (loop [[line & rest_lines] lines
+         sum 0]
+    (if (some? line)
+      (recur rest_lines (+ sum (Integer/parseInt line)))
+      sum
+      )))
 
 (defn -main [& args] ;; & for variadic
-  ;; filename & args destructures the head off
-  (let [[filename & args] args]
-    (println filename)
-    (read filename)
-  )
+  (let [lines (get-lines args)]
+    (println (sum-lines lines)))
 )
